253b0cfe3d3574af6cad83e566c24e146012c442
Dave Ankin <alexanderankin@gmail.com>
2015-04-12 03:06:28 -0400

    put into bs grid

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 500, Phaser.AUTO, 'gameCanvasContainer', { preload: preload, create: create, update: update });
 
 var colors = new tracking.ColorTracker(['yellow']);
 
